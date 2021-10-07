5b34b16728f8bb3d7b0a341638b83817b5b522e9
Martin <teszt1>
2021-10-07 12:46:40 +0200

    LED teszt

--- RGBLed/RGBLed.ino
+++ RGBLed/RGBLed.ino
@@ -1,12 +1,27 @@
-int r=9
-int b=10
-int g=11
+int r=9;
+int b=10;
+int g=11;
 void setup() {
-  
+  pinMode(r,OUTPUT);
+  pinMode(b,OUTPUT);
+  pinMode(g,OUTPUT);
 
+  digitalWrite(r,1);
+  delay(500);
+
+  digitalWrite(r,0);
+  digitalWrite(b,1);
+  delay(500);
+
+  digitalWrite(b,0);
+  digitalWrite(g,1);
+  delay(500);
+
+  digitalWrite(r,1);
+  digitalWrite(b,1);
+  
 }
 
 void loop() {
  
-
 }
